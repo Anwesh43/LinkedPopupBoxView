c4b87530e6ea3f7cf3ddc886aca09a67edb49025
Anwesh43 <anweshthecool0@gmail.com>
2019-08-04 12:48:35 +0530

    adding PopupBoxView class

--- popupboxview/src/main/java/com/anwesh/uiprojects/popupboxview/PopupBoxView.kt
+++ popupboxview/src/main/java/com/anwesh/uiprojects/popupboxview/PopupBoxView.kt
@@ -54,3 +54,21 @@ fun Canvas.drawPBNode(i : Int, scale : Float, paint : Paint) {
     drawPopup(i, size, scale, paint)
     restore()
 }
+
+class PopupBoxView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
