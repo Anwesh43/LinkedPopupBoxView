1d95c3a22f3cb47e4521c614e11ac11fa8737b62
Anwesh43 <anweshthecool0@gmail.com>
2019-08-04 12:33:58 +0530

    adding constants and libraries

--- popupboxview/src/main/java/com/anwesh/uiprojects/popupboxview/PopupBoxView.kt
+++ popupboxview/src/main/java/com/anwesh/uiprojects/popupboxview/PopupBoxView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.popupboxview
 /**
  * Created by anweshmishra on 04/08/19.
  */
+
+import android.view.View
+import android.app.Activity
+import android.content.Context
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val parts : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#01579B")
+val backColor : Int = Color.parseColor("#BDBDBD")
