8d068d7151491207848420bb86a70408be8109e2
Anwesh43 <anweshthecool0@gmail.com>
2019-08-04 13:01:30 +0530

    adding PopupBox class

--- popupboxview/src/main/java/com/anwesh/uiprojects/popupboxview/PopupBoxView.kt
+++ popupboxview/src/main/java/com/anwesh/uiprojects/popupboxview/PopupBoxView.kt
@@ -163,4 +163,28 @@ class PopupBoxView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class PopupBox(var i : Int) {
+
+        private val root : PBNode = PBNode(0)
+        private var curr : PBNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
